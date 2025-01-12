b55cb6fa93417a348703af737eaf88fa01dfe1af
andre <andreamusholin@gmail.com>
2025-01-12 08:03:20 +0700

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,7 +30,7 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="robotic.vacuum" color={color} />,
         }}
       />
       <Tabs.Screen
